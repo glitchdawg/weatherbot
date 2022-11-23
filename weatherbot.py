b901d3eb997074b702f50acdfdd321a15b0a47ad
glitchdawg <shivavayuputra8512@GMail.com>
2022-11-23 20:03:12 +0530

    security issues

--- weatherbot.py
+++ weatherbot.py
@@ -1,10 +1,9 @@
 import requests
 from pprint import pprint
-
-API_KEY = 'c11d57999051f03cba808719c9653aeb'
+import config
 
 city=input("Enter a city: ")
-base_url='http://api.openweathermap.org/data/2.5/weather?appid='+API_KEY+'&q='+city
+base_url='http://api.openweathermap.org/data/2.5/weather?appid='+config.API_KEY+'&q='+city
 
 weather_data = requests.get(base_url).json()
 
